747df61ba3d76f37b3cf3aeab794159fd1d69e45
Chu Mai <chuthimai18245@gmail.com>
2023-09-25 20:57:41 +0700

    chỉnh sửa database phần State

--- main.py
+++ main.py
@@ -2,28 +2,18 @@ from flask import Flask, redirect, url_for, render_template, request, session
 from datetime import timedelta
 from flask.helpers import flash
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
 from os import path
-
-from werkzeug.security import generate_password_hash, \
-    check_password_hash
-
-# import mysql.connector
-# from mysql.connector import errorcode
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 app = Flask(  __name__ )
 app.config["SECRET_KEY"] = "my-secret-key"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///user.db"
-# app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+mysqlconnector://chuong:chuong@localhost/test"
-
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.permanent_session_lifetime = timedelta(minutes=1)
 
 db = SQLAlchemy(app)
 
-
-# db.app = app
 
 class User(db.Model):
     # _tablename_ = 'users'
@@ -36,7 +26,7 @@ class User(db.Model):
     rank_id = db.Column(db.Integer)
     user_rela = db.relationship('States', backref=db.backref('user', lazy=True))
 
-    def __init__(self, user_name, email, password, bio, avatar=0, rank_id=0):
+    def __init__(self, user_name, email, password, bio, avatar=0, rank_id=2):
         self.user_name = user_name
         self.email = email
         self.password = generate_password_hash(password)
@@ -52,7 +42,7 @@ class States(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     blog_id = db.Column(db.Integer, db.ForeignKey('blog_post.id'))
-    comment = db.Column(db.String(1014))
+    comment = db.Column(db.Text)
 
 
 class BlogPost(db.Model):
@@ -61,14 +51,8 @@ class BlogPost(db.Model):
     date = db.Column(db.DateTime, nullable=False)
     title = db.Column(db.String(1014), nullable=False)
     subtitle = db.Column(db.String(1014))
-    body = db.Column(db.String(10845), nullable=False)
+    body = db.Column(db.Text, nullable=False)
     BlogPost_rela = db.relationship('States', backref=db.backref('BlogPost', lazy=True))
-
-
-with app.app_context():
-    if not path.exists("user.db"):
-        db.create_all()
-        print("Created database!")
 
 
 @app.route('/select/user')
@@ -82,17 +66,15 @@ def select():
     return html
 
 
-@app.route('/insert', methods=["GET"])
+# @app.route('/insert', methods=["GET"])
 def insert_test():
-    new_user = User('username1', 'trieuquoccuong321@gmail.com', '12345678', 'bio');
+    new_user = User('Admin', 'admin@gmail.com', '12345678', 'Admin');
     db.session.add(new_user)
     db.session.commit()
     return 'Da insert du lieu'
 
 
-@app.route('/test_checkpass')
 def check_pass():
-    # s = Session();
     user = User.query.filter_by(user_name='username1').first()
     if user:
         if user.check_password('12345678') == True:
@@ -101,3 +83,10 @@ def check_pass():
             return 'sai'
     else:
         return 'khong co username như v'
+
+
+with app.app_context():
+    if not path.exists("user.db"):
+        db.create_all()
+        insert_test()
+        print("Created database!")
